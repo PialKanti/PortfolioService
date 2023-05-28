abb0f333be25d0421f4810949a85e77b7107c4bb
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-05-28 13:39:43 +0600

    Added Interfaces for Application layer

--- src/Core/Application/PortfolioService.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PortfolioService.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Core/Application/PortfolioService.Application/Interfaces/IExperienceRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioService.Application.Interfaces
+{
+    public interface IExperienceRepository<T>
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAll();
+    }
+}
