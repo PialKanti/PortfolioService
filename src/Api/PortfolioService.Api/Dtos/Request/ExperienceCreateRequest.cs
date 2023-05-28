b5a8309becf8a50113bde2784a13d218a9226c3c
Pial Kanti Samadder <pialkanti2012@gmail.com>
2023-05-28 23:58:01 +0600

    Added Required annotation to Request dto

--- src/Api/PortfolioService.Api/Dtos/Request/ExperienceCreateRequest.cs
+++ src/Api/PortfolioService.Api/Dtos/Request/ExperienceCreateRequest.cs
@@ -1,14 +1,21 @@
 ﻿using PortfolioService.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace PortfolioService.Api.Dtos.Request
 {
     public class ExperienceCreateRequest
     {
-        public string Title { get; set; }
-        public string EmploymentType { get; set; }
-        public Company Company { get; set; }
-        public IEnumerable<string> Responsibilities { get; set; }
-        public IEnumerable<string> Skills { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? EmploymentType { get; set; }
+        [Required]
+        public Company? Company { get; set; }
+        [Required]
+        public IEnumerable<string>? Responsibilities { get; set; }
+        [Required]
+        public IEnumerable<string>? Skills { get; set; }
+        [Required]
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
     }
